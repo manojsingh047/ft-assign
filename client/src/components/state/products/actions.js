cc89447238346651edbdb1370abd240f5f2ba96b
Manoj Singh <manojsingh047@gmail.com>
2022-02-06 00:27:15 +0530

    Delete client/src/components/state directory

--- client/src/components/state/products/actions.js
+++
@@ -1,5 +0,0 @@
-export const SET_PRODUCTS_CNT = "products/SET_PRODUCTS_CNT";
-
-export const setProductsCnt = () => ({
-  type: SET_PRODUCTS_CNT,
-});
--- client/src/components/state/reducers.js
+++
@@ -1,9 +0,0 @@
-import cartReducer from "./cart/reducer";
-import productsReducer from "./products/reducer";
-
-const reducers = ({ cartState, productsState }, action) => ({
-  cartState: cartReducer(cartState, action),
-  productsState: productsReducer(productsState, action),
-});
-
-export default reducers;
